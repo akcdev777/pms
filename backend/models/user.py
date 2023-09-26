db904e0aa542e3536cbdc0366aa30ce1433ecf60
Atanas <akc00018@students.stir.ac.uk>
2023-09-26 20:41:33 +0100

    User class defined

---
+++ backend/models/user.py
@@ -0,0 +1,57 @@
+## User class
+## Author: Atanas Chelibashki
+## Date: 26/09/2023
+## Version 1.0
+## Description: This class is used to create a user object
+
+class User:
+    def __init__(self, username, password, first_name, last_name, email):
+        self.username = username
+        self.password = password
+        self.fname = first_name
+        self.lname = last_name
+        self.email = email
+
+    # Add other methods as needed
+
+    def __str__(self):
+        return f"{self.username} {self.password} {self.fname} {self.email}"
+
+    def __repr__(self):
+        return f"{self.username} {self.password} {self.fname} {self.email}"
+
+    def get_fname(self):
+        return self.fname
+
+    def get_lname(self):
+        return self.lname
+
+    def get_email(self):
+        return self.email
+
+    def get_username(self):
+        return self.username
+
+    def get_password(self):
+        return self.password
+
+    def get_full_name(self):
+        return self.fname + "" + self.lname
+
+    def set_fname(self, fname):
+        self.fname = fname
+
+    def set_lname(self, lname):
+        self.lname = lname
+
+    def set_email(self, email):
+        self.email = email
+
+    def set_username(self, username):
+        self.username = username
+
+    def set_password(self, password):
+        self.password = password
+
+
+
